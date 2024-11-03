018e4c18f0c58a10304f774fb400092b56b2b5cc
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-11-03 18:05:06 +0400

    creating contact page

---
+++ src/app/(mainDashboard)/contact/page.tsx
@@ -0,0 +1,14 @@
+
+import ContactForm from '@/components/_contact/ContactForm'
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div className="mt-5">
+      <h2 className="section-title-big text-center">ƏLAQƏ</h2>
+      <ContactForm />
+    </div>
+  );
+}
+
+export default ContactPage
