5345eaa9ea879c7e63f21027258c3744ecce4aea
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-11-03 18:12:10 +0400

    adding all ready components to home page

--- src/app/(mainDashboard)/page.tsx
+++ src/app/(mainDashboard)/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import HeroIndexBanner from "@/components/_common/HeroIndexBanner";
+import IntroVideoSection from "@/components/_common/IntroVideoSection";
+import ContactInfo from "@/components/_home/ContactInfo/ContactInfo";
+import OurServices from "@/components/_home/Our-Services/OurServices";
+import Partnerships from "@/components/_home/Partnerships/Partnerships";
+import React from "react";
 
 const HomeLandingPage = () => {
   return (
-    <div>HomeLandingPage</div>
-  )
-}
+    <main className="">
+      <HeroIndexBanner />
+      <OurServices />
+      <IntroVideoSection />
+      <Partnerships />
+      <ContactInfo/>
+    </main>
+  );
+};
 
-export default HomeLandingPage
+export default HomeLandingPage;
