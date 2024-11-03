40a383840bbd55acb403bd6782b2f498c0745866
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-11-03 18:21:48 +0400

    solving build and ts errors

--- src/components/_common/MainMenuDropdown.tsx
+++ src/components/_common/MainMenuDropdown.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
-import React, { useRef } from "react";
-import { RxHamburgerMenu } from "react-icons/rx";
+import React from "react";
 import { motion } from "framer-motion";
 
 interface ILink {
