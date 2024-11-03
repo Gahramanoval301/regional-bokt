00404d656c40a3a37ab59c3e6e5705a16b836a75
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-11-03 18:11:25 +0400

    seperating array datas

---
+++ src/data/Partnerships.ts
@@ -0,0 +1,26 @@
+export const partnerships = [
+    {
+        id: 0,
+        imgUrl:"/partnerships/merkezi-bank.png"
+    },
+    {
+        id: 1,
+        imgUrl:"/partnerships/akb.png"
+    },
+    {
+        id: 2,
+        imgUrl:"/partnerships/beynelxalq-bank.png"
+    },
+    {
+        id: 3,
+        imgUrl:"/partnerships/xalq-bank.png"
+    },
+    {
+        id: 4,
+        imgUrl:"/partnerships/azerpoct.png"
+    },
+    {
+        id: 5,
+        imgUrl:"/partnerships/azer-turk-bank.png"
+    },
+]
