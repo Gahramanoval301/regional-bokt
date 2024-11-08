a1b49cdf050ea0f9cc49d385daff3798754ee618
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-11-08 18:24:36 +0400

    rendering each form -  validation, initialValues, inputs, onSubmits

---
+++ src/components/forms/Form3.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Field, Form, Formik, ErrorMessage } from "formik";
+import MainButton from "../_common/MainButton";
+import { initialValues, inputDatas3, validationScheme } from "@/data/forms/form3";
+
+
+const Form3 = () => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationScheme}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          console.log(values);
+          actions.resetForm();
+        }, 1000);
+      }}
+    >
+      <Form>
+        <div className="grid gap-4 grid-cols-2">
+          {inputDatas3.map(({ name, type, className, label, placeholder }) => {
+            return (
+              <>
+                <div className={`text-black ${className}`} key={name}>
+                  <p className="font-semibold text-lg my-1">{label}*</p>
+                  <Field
+                    className="custom-input-2"
+                    name={name}
+                    placeholder={placeholder}
+                    type={type}
+                  />
+                  <ErrorMessage
+                    name={name}
+                    component="div"
+                    className="error-message"
+                  />
+                </div>
+              </>
+            );
+          })}
+          <div className="col-span-2">
+            <label className="text-sm pt-1">
+              <Field className="mr-1" name="checkTrue" type="checkbox" />
+              Məlumatlarımın doğruluğunu təsdiq edir,kredit öhdəliklərim barədə
+              məlumatları AKB kredit reyestrindən,şəxsi və iş yerim üzrə
+              məlumatları isə “ASAN Finans” sistemi vasitəsilə əldə edilməsinə
+              icazə verirəm.
+            </label>
+            <ErrorMessage
+              name={"checkTrue"}
+              component="div"
+              className="error-message"
+            />
+          </div>
+          <MainButton
+            type="submit"
+            title="Sifariş Et"
+            styles=" col-span-2 bg-secondary-dark text-primary mt-4"
+          />
+        </div>
+      </Form>
+    </Formik>
+  );
+};
+
+export default Form3;
