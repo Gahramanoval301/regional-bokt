1756aa59de3fd8381e55e2cb94343089b07dab74
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-11-16 00:40:10 +0400

    seperating the component and correcting on class names

---
+++ src/components/_about/AboutContent.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const AboutContent = () => {
+  return (
+    <div className="about-general section-gap ">
+      <div className="leftside">
+        <span className="text-accent-violet">&quot;Regional&quot;</span> BOKT
+        MMC 10 Aprel 2014 tarixində təsis edilmiş, 18 Aprel 2014 tarixində
+        Dövlət qeydiyyatından keçmişdir. 1 Avqust 2014-cü ildə Mərkəzi Bankın
+        verdiyi BKT - 26 saylı lisenziyası əsasında, Azərbaycan Respublikasının
+        Bank Olmayan Kredit Təşkilatları haqqında qanuna və Mərkəzi Bankın
+        göstəriş və təlimatlarına uyğun olaraq fəaliyyət göstərir. Təşkilat,
+        kiçik və orta sahibkarlıq subyektlərinin, eləcə də fiziki şəxslərin
+        kredit məhsullarına olan ehtiyacını ödəyən rəqabət qabiliyyətli, peşəkar
+        komandaya malik kredit təşkilatıdır.
+      </div>
+      <div className="text-right">
+        Müştərilərlə{" "}
+        <span className="text-accent-violet">
+          qarşılıqlı hörmət, əlçatanlıq və operativlik prinsipləri əsasında
+          xidmət göstərərək, şirkət bir çox nüfuzlu təşkilatlarla, maliyyə
+          institutları ilə, banklar və dövlət strukturları ilə sıx əməkdaşlıq
+          edir. Regional” BOKT öz fəaliyyətini korporativ idarəetmə və
+          hesabatlılıq siyasəti əsasında tənzimləyir. Təşkilatımızın illik
+          fəaliyyətinə dair hesabatlar rəsmi olaraq kənar auditor xidməti
+          tərəfindən təsdiqlənir və kütləvi İnformasiya vasitələrində
+          yayımlanır.
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default AboutContent
