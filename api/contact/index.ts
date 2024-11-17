f8912d7fea510c1a46f8e20292e78898d553291e
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-11-18 00:35:29 +0400

    api contact

---
+++ api/contact/index.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL + "/api/Contact";
+
+export const sendContactForm = async (newContactData: any) => {
+    const res = await axios.post(`${BASE_URL}/create-form`, newContactData);
+    return res;
+}
